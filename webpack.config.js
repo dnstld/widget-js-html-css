f222b10a1fee1d9f80791dfd1affc2dfff4f5c66
Denis Toledo <contato@denistoledo.com.br>
2022-10-27 15:47:39 +0200

    added babel as module to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,31 @@ module.exports = (env) => {
       minimize: !isDevBuild
     },
     mode: isDevBuild ? 'development' : 'production',
+    module: {
+      rules: [
+        {
+          test: /\.js$/i,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', {
+                  'targets': {
+                      'ie': '11',
+                      'esmodules': true
+                  },
+                  // makes usage of @babel/polyfill because of IE11
+                  // there is at least async functions and for...of
+                  useBuiltIns: 'usage',
+                  corejs: '3.0.0'
+                }]
+              ]
+            }
+          }
+        }
+      ]
+    },
     resolve: {
       extensions: ['*', '.js']
     }
